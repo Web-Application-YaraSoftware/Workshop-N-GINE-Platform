4ab65d2d463847f604389f0955b96f398878ac7e
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 17:31:58 -0500

    feat(profile): add IProfileCommandService and ProfileCommandService
    - Added IProfileCommandService interface to define profile command operations.
    - Implemented ProfileCommandService to handle profile command operations.

---
+++ YARA.WorkshopNGine.API/Profiles/Domain/Services/IProfileCommandService.cs
@@ -0,0 +1,10 @@
+﻿using YARA.WorkshopNGine.API.Profiles.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Profiles.Domain.Model.Commands;
+
+namespace YARA.WorkshopNGine.API.Profiles.Domain.Services;
+
+public interface IProfileCommandService
+{
+    Task<Profile?> Handle(CreateProfileCommand command);
+    Task<Profile?> Handle(long profileId, UpdateProfileCommand command);
+}
