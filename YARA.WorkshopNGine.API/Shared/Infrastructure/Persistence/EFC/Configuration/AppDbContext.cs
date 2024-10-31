c1111475fc1e87e0839d09ef4c668d274b5edef5
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 14:46:26 -0500

    feat(profile): configure profile entity in app db context

--- YARA.WorkshopNGine.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ YARA.WorkshopNGine.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using YARA.WorkshopNGine.API.Profiles.Domain.Model.Aggregates;
 using YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -11,6 +12,19 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         base.OnModelCreating(builder);
         
         // Place here the configuration of the entities
+        
+        // Profile Context
+        builder.Entity<Profile>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.FirstName).HasMaxLength(100);
+            entity.Property(p => p.LastName).HasMaxLength(100);
+            entity.Property(p => p.Dni).IsRequired();
+            entity.Property(p => p.Email).HasMaxLength(200);
+            entity.Property(p => p.Age);
+            entity.Property(p => p.Location).HasMaxLength(100);
+            entity.Property(p => p.UserId).IsRequired();
+        });
         
         // Apply SnakeCase Naming Convention
         builder.UseSnakeCaseWithPluralizedTableNamingConvention();
