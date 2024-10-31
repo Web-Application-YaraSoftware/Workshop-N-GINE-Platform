6fad85c520895fd19392158b2cdf36a1e9cee54e
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 14:50:15 -0500

    feat(profile): add IProfileRepository interface and implement ProfileRepository class
    - Created IProfileRepository interface to define repository methods for Profile entity.
    - Implemented ProfileRepository class with asynchronous methods FindByDniAsync and FindByUserIdAsync.

---
+++ YARA.WorkshopNGine.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using YARA.WorkshopNGine.API.Profiles.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
+
+namespace YARA.WorkshopNGine.API.Profiles.Domain.Repositories;
+
+public interface IProfileRepository : IBaseRepository<Profile>
+{
+    Task<Profile?> FindByDniAsync(int dni);
+    
+    Task<Profile?> FindByUserIdAsync(long userId);
+    
+}
