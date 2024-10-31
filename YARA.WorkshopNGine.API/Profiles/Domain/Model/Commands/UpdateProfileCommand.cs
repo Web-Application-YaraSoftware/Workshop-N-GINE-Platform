28bb5013bf4cc6be9d70e660871a0cd6cb02410d
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 17:28:58 -0500

    feat(profile): add CreateProfileCommand and UpdateProfileCommand
    - Added CreateProfileCommand to handle profile creation.
    - Added UpdateProfileCommand to handle profile updates.

---
+++ YARA.WorkshopNGine.API/Profiles/Domain/Model/Commands/UpdateProfileCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace YARA.WorkshopNGine.API.Profiles.Domain.Model.Commands;
+
+public record UpdateProfileCommand(long Id, string FirstName, string LastName, int Dni, string Email, int Age, string Location, long UserId);
