677c3d71b382289ad03ad60eb048a9439c4c25c8
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-31 02:44:16 -0500

    chore: implemented persistence base configuration

---
+++ YARA.WorkshopNGine.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+namespace YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Place here the configuration of the entities
+        
+        // Apply SnakeCase Naming Convention
+        builder.UseSnakeCaseWithPluralizedTableNamingConvention();
+    }
+}
---
+++ YARA.WorkshopNGine.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
+using YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CompleteAsync() => await context.SaveChangesAsync();
+}
