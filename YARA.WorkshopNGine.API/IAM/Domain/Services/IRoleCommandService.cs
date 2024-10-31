ce9ec4a96de8f2ead0e80ae4906ed6ee07bcb916
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-31 17:13:14 -0500

    feat(IAM): implement command and query services for Role and User management
    - Added `RoleCommandService` to handle role seeding via `SeedRolesCommand`.
    - Implemented `UserCommandService` with `SignUpCommand` and `SignInCommand` handling for user creation and authentication.
    - Developed `UserQueryService` to retrieve users by role and workshop with `GetAllUsersByRoleAndWorkshopQuery`.

---
+++ YARA.WorkshopNGine.API/IAM/Domain/Services/IRoleCommandService.cs
@@ -0,0 +1,8 @@
+﻿using YARA.WorkshopNGine.API.IAM.Domain.Model.Commands;
+
+namespace YARA.WorkshopNGine.API.IAM.Domain.Services;
+
+public interface IRoleCommandService
+{
+    Task Handle(SeedRolesCommand command);
+}
