e9259a2b25f85a1fefeda707408f636615522fef
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 17:42:13 -0500

    feat(profile): add CreateUpdateProfileCommandFromResourceAssembler, UpdateProfileResource, CreateProfileCommandFromResourceAssembler, and CreateProfileResource

---
+++ YARA.WorkshopNGine.API/Profiles/interfaces/REST/Transform/CreateProfileCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using YARA.WorkshopNGine.API.Profiles.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Profiles.interfaces.REST.Resources;
+
+namespace YARA.WorkshopNGine.API.Profiles.interfaces.REST.Transform;
+
+public class CreateProfileCommandFromResourceAssembler
+{
+    public static CreateProfileCommand ToCommandFromResource(CreateProfileResource resource)
+    {
+        return new CreateProfileCommand(resource.FirstName, resource.LastName, resource.Dni, resource.Email,
+            resource.Age, resource.Location, resource.UserId);
+    }
+}
