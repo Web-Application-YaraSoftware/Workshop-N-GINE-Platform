58e2cf14fcb440c60e32bd438dbd88cabda6d4c4
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-31 02:12:53 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YARA Workshop N-Gine Platform
+
+## Summary
+YARA Workshop N-Gine Platform API Application, made with Microsoft C#, ASP.NET Core, Entity Framework Core and MySQL persistence. It also illustrates open-api documentation configuration and integration with Swagger UI.
+
---
+++ YARA.WorkshopNGine.API/Program.cs
@@ -0,0 +1,73 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+/*
+ builder.Services.AddDbContext<AppDbContext>(options =>
+ {
+    if(connectionString != null)
+      if (builder.Environment.IsDevelopment())
+      options.UseMySQL(connectionString)
+      .LogTo(Console.WriteLine, LogLevel.Information);
+      .EnableSensitiveDataLogging()
+      .EnableDetailedErrors();
+      else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Error);
+        .EnableDetailedErrors();
+ });
+ */
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    c => { c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "YARA.WorkshopNGine.API",
+        Version = "v1",
+        Description = "YARA WorkshopNGine Platform API",
+        TermsOfService = new Uri("https://yara-workshop.com/tos"),
+        Contact = new OpenApiContact {Name = "YARA WorkshopNGine", Email = "contact@yara.com"},
+        License = new OpenApiLicense
+        {
+            Name = "Apache 2.0",
+            Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+        }
+    }); });
+
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+var app = builder.Build();
+
+// Verify Database Objects are Created
+/*
+ using (var scope = app.Services.CreateScope())
+ {
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+ }
+ */
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
