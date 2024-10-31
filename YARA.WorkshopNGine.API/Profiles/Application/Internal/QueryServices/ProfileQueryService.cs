ded6808b2d486fc8095f2f899eda83ae6dcd9231
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-31 15:00:19 -0500

    feat(profile): add IProfileQueryService interface and implement ProfileQueryService class
    - Created IProfileQueryService interface to define query service methods for Profile entity.
    - Implemented ProfileQueryService class with methods to handle profile queries.

---
+++ YARA.WorkshopNGine.API/Profiles/Application/Internal/QueryServices/ProfileQueryService.cs
@@ -0,0 +1,24 @@
+﻿using YARA.WorkshopNGine.API.Profiles.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Profiles.Domain.Model.Queries;
+using YARA.WorkshopNGine.API.Profiles.Domain.Repositories;
+using YARA.WorkshopNGine.API.Profiles.Domain.Services;
+
+namespace YARA.WorkshopNGine.API.Profiles.Application.Internal.QueryServices;
+
+public class ProfileQueryService(IProfileRepository profileRepository) : IProfileQueryService
+{
+    public async Task<Profile?> Handle(GetProfileByIdQuery query)
+    {
+        return await profileRepository.FindByIdAsync(query.ProfileId);
+    }
+
+    public async Task<Profile?> Handle(GetProfileByDniQuery query)
+    {
+        return await profileRepository.FindByDniAsync(query.Dni);
+    }
+
+    public async Task<Profile?> Handle(GetProfileByUserIdQuery query)
+    {
+        return await profileRepository.FindByUserIdAsync(query.UserId);
+    }
+}
