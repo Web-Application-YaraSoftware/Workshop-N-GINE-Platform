4722a41f9f5c012fa352c982825572b78d8e657d
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-31 17:17:36 -0500

    feat(IAM): add resources and transformers for authentication and user registration
    - Added `AuthenticatedUserResource` for user response structure.
    - Created transformers for `User` to `AuthenticatedUserResource`.
    - Added `SignInResource` and `SignUpResource` with transformers to convert to `SignInCommand` and `SignUpCommand`.

---
+++ YARA.WorkshopNGine.API/IAM/Interfaces/REST/Transform/SignUpCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using YARA.WorkshopNGine.API.IAM.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.IAM.Interfaces.REST.Resources;
+
+namespace YARA.WorkshopNGine.API.IAM.Interfaces.REST.Transform;
+
+public static class SignUpCommandFromResourceAssembler
+{
+    public static SignUpCommand ToCommandFromResource(SignUpResource resource)
+    {
+        return new SignUpCommand(resource.Username, resource.Password, resource.RoleId, resource.WorkshopId);
+    }
+}
