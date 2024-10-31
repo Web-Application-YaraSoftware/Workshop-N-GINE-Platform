8bced1fcb6e0f2cb837bd630af79698dfb0be24e
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-31 17:07:54 -0500

    feat(IAM): add Role and User repositories with essential queries
    - Implemented `IRoleRepository` with `ExistsByName` method.
    - Added `IUserRepository` with methods for user retrieval, password verification, and role/workshop filtering.
    - Created `RoleRepository` to check if a role exists by name.
    - Developed `UserRepository` with `FindByUsernameAsync`, `ExistsByUsername`, `VerifyPassword`, and `FindAllByRoleAndWorkshopAsync`.

---
+++ YARA.WorkshopNGine.API/IAM/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using YARA.WorkshopNGine.API.IAM.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
+
+namespace YARA.WorkshopNGine.API.IAM.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> FindByUsernameAsync(string username);
+    
+    bool ExistsByUsername(string username);
+    
+    bool VerifyPassword(string username, string password);
+    
+    Task<IEnumerable<User>> FindAllByRoleAndWorkshopAsync(long workshopId, long roleId);
+}
--- YARA.WorkshopNGine.API/Shared/Domain/Repositories/IBaseRepository.cs
+++ YARA.WorkshopNGine.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -3,7 +3,7 @@ namespace YARA.WorkshopNGine.API.Shared.Domain.Repositories;
 public interface IBaseRepository<TEntity>
 {
     Task AddAsync(TEntity entity);
-    Task<TEntity?> FindByIdAsync(int id);
+    Task<TEntity?> FindByIdAsync(long id);
     void Update(TEntity entity);
     void Remove(TEntity entity);
     Task<IEnumerable<TEntity>> ListAsync();
