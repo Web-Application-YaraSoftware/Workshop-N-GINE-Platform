8db81138161e8c6272a84c8afee53b5aa5e248f7
Eric <erich56000@gmail.com>
2024-10-31 20:18:59 -0500

    feat(interfaces-REST): add resources and transformation utilities for notifications
    - Created `NotificationResource` and `NotificationStateResource` to structure REST API responses for notifications and notification states.
    - Added `NotificationResourceFromEntityAssembler` to convert `Notification` entities into `NotificationResource`.
    - Added `NotificationStateResourceFromEntityAssembler` to convert `NotificationState` entities into `NotificationStateResource`.
    - Enabled clear transformation between domain entities and API resources.

---
+++ YARA.WorkshopNGine.API/CommunicationManagement/Interfaces/REST/Resources/NotificationStateResource.cs
@@ -0,0 +1,5 @@
+﻿using YARA.WorkshopNGine.API.CommunicationManagement.Domain.Model.ValueoObjects;
+
+namespace YARA.WorkshopNGine.API.CommunicationManagement.Interfaces.REST.Resources;
+
+public record NotificationStateResource( int Id, ENotificationState Name);
