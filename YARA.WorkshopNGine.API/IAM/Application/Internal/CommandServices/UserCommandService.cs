131834259c96c8e7e1f591535dbe3ddbc8ba2009
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 16:59:13 -0500

    feat: add command handling for creating users with specific roles
    - Added methods to `IUserCommandService` interface for handling client and mechanic user creation.
    - Implemented methods in `UserCommandService` class for handling client and mechanic user creation.

--- YARA.WorkshopNGine.API/IAM/Application/Internal/CommandServices/UserCommandService.cs
+++ YARA.WorkshopNGine.API/IAM/Application/Internal/CommandServices/UserCommandService.cs
@@ -1,5 +1,6 @@
 ﻿using YARA.WorkshopNGine.API.IAM.Domain.Model.Aggregates;
 using YARA.WorkshopNGine.API.IAM.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.IAM.Domain.Model.ValueObjects;
 using YARA.WorkshopNGine.API.IAM.Domain.Repositories;
 using YARA.WorkshopNGine.API.IAM.Domain.Services;
 using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
@@ -30,5 +31,41 @@ public class UserCommandService(IUserRepository userRepository, IUnitOfWork unit
         if (user is null || !userRepository.VerifyPassword(command.Username, command.Password))
             throw new Exception("Invalid username or password.");
         return user;
+    }
+
+    public async Task<User> Handle(CreateUserWithRoleClientCommand command)
+    {
+        if (userRepository.ExistsByUsername(command.Username))
+            throw new Exception($"Username {command.Username} already exists.");
+        const long role = (long)Roles.Client;
+        var user = new User(command.Username, command.Password, role, command.WorkshopId);
+        try
+        {
+            await userRepository.AddAsync(user);
+            await unitOfWork.CompleteAsync();
+            return user;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("An error occurred while trying to create the user.", e);
+        }
+    }
+
+    public async Task<User> Handle(CreateUserWithRoleMechanicCommand command)
+    {
+        if (userRepository.ExistsByUsername(command.Username))
+            throw new Exception($"Username {command.Username} already exists.");
+        const long role = (long)Roles.Mechanic;
+        var user = new User(command.Username, command.Password, role, command.WorkshopId);
+        try
+        {
+            await userRepository.AddAsync(user);
+            await unitOfWork.CompleteAsync();
+            return user;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("An error occurred while trying to create the user.", e);
+        }
     }
 }
