4f556e5851451a2e04ea5acc210ce262a6ece00f
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-08 03:14:59 -0500

    feat(service): add resource assemblers for Intervention commands and entities
    - Added `CreateInterventionCommandFromResourceAssembler` class to convert `CreateInterventionResource` to `CreateInterventionCommand`.
    - Added `InterventionResourceFromEntityAssembler` class to convert `Intervention` entity to `InterventionResource`.
    - Added `UpdateInterventionCommandFromResourceAssembler` class to convert `UpdateInterventionResource` to `UpdateInterventionCommand`.

---
+++ YARA.WorkshopNGine.API/Service/Interfaces/REST/Transform/CreateInterventionCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Service.Interfaces.REST.Resources;
+
+namespace YARA.WorkshopNGine.API.Service.Interfaces.REST.Transform;
+
+public class CreateInterventionCommandFromResourceAssembler
+{
+    public static CreateInterventionCommand ToCommandFromResource(CreateInterventionResource resource)
+    {
+        return new CreateInterventionCommand(resource.WorkshopId, resource.VehicleId, resource.MechanicLeaderId, resource.Description);
+    }
+}
