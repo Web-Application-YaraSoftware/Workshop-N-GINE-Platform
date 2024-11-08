9d257f20641f438f331d0cc0d96dcc7131412ad1
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 23:07:31 -0500

    feat(service): implement user and profile creation commands
    - Added methods to handle the creation of clients and mechanics.
    - Integrated services for user and profile creation.

--- YARA.WorkshopNGine.API/Service/Application/Internal/CommandServices/WorkshopCommandService.cs
+++ YARA.WorkshopNGine.API/Service/Application/Internal/CommandServices/WorkshopCommandService.cs
@@ -1,12 +1,14 @@
-﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
+﻿using YARA.WorkshopNGine.API.Service.Application.Internal.OutboundServices.ACL;
+using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
 using YARA.WorkshopNGine.API.Service.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Service.Domain.Model.ValueObjects;
 using YARA.WorkshopNGine.API.Service.Domain.Repositories;
 using YARA.WorkshopNGine.API.Service.Domain.Services;
 using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
 
 namespace YARA.WorkshopNGine.API.Service.Application.Internal.CommandServices;
 
-public class WorkshopCommandService(IWorkshopRepository workshopRepository, IUnitOfWork unitOfWork): IWorkshopCommandService
+public class WorkshopCommandService(IWorkshopRepository workshopRepository, IUnitOfWork unitOfWork, ExternalIamService externalIamService, ExternalProfileService externalProfileService): IWorkshopCommandService
 {
     public async Task<Workshop?> Handle(CreateWorkshopCommand command)
     {
@@ -49,5 +51,33 @@ public class WorkshopCommandService(IWorkshopRepository workshopRepository, IUni
             Console.WriteLine($"An error occurred while updating the workshop: {e.Message}");
             return null;
         }
+    }
+
+    public async Task<UserId?> Handle(long workshopId, CreateClientCommand command)
+    {
+        if(!workshopRepository.ExistsById(workshopId))
+            throw new Exception($"Workshop with the id '{workshopId}' does not exist.");
+        var userId = await externalIamService.CreateUserWithRoleClientAsync(command.Email, command.Dni.ToString(), workshopId);
+        if (userId == null)
+            throw new Exception("An error occurred while creating the user.");
+        // TODO: If the creation of the profile fails, the user should be deleted.
+        var profileId = await externalProfileService.CreateProfileAsync(command.FirstName, command.LastName, command.Dni, command.Email, command.Age, command.Location, userId.Id);
+        if (profileId == null)
+            throw new Exception("An error occurred while creating the profile.");
+        return userId;
+    }
+
+    public async Task<UserId?> Handle(long workshopId, CreateMechanicCommand command)
+    {
+        if(!workshopRepository.ExistsById(workshopId))
+            throw new Exception($"Workshop with the id '{workshopId}' does not exist.");
+        var userId = await externalIamService.CreateUserWithRoleMechanicAsync(command.Email, command.Dni.ToString(), workshopId);
+        if (userId == null)
+            throw new Exception("An error occurred while creating the user.");
+        // TODO: If the creation of the profile fails, the user should be deleted.
+        var profileId = await externalProfileService.CreateProfileAsync(command.FirstName, command.LastName, command.Dni, command.Email, command.Age, command.Location, userId.Id);
+        if (profileId == null)
+            throw new Exception("An error occurred while creating the profile.");
+        return userId;
     }
 }
--- YARA.WorkshopNGine.API/Service/Domain/Services/IWorkshopCommandService.cs
+++ YARA.WorkshopNGine.API/Service/Domain/Services/IWorkshopCommandService.cs
@@ -1,5 +1,6 @@
 ﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
 using YARA.WorkshopNGine.API.Service.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Service.Domain.Model.ValueObjects;
 
 namespace YARA.WorkshopNGine.API.Service.Domain.Services;
 
@@ -7,4 +8,8 @@ public interface IWorkshopCommandService
 {
     Task<Workshop?> Handle(CreateWorkshopCommand command);
     Task<Workshop?> Handle(long workshopId, UpdateWorkshopCommand command);
+    
+    Task<UserId?> Handle(long workshopId, CreateClientCommand command);
+    
+    Task<UserId?> Handle(long workshopId, CreateMechanicCommand command);
 }
