b0b2a4164f5489077f304470782faf2443cc6ab0
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 23:09:16 -0500

    feat(service): add resources and assemblers for client and mechanic creation
    - Added `CreateClientResource` and `CreateMechanicResource` records.
    - Implemented assemblers to convert resources to commands.

---
+++ YARA.WorkshopNGine.API/Service/Interfaces/REST/Transform/CreateClientCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Service.Interfaces.REST.Resources;
+
+namespace YARA.WorkshopNGine.API.Service.Interfaces.REST.Transform;
+
+public class CreateClientCommandFromResourceAssembler
+{
+    public static CreateClientCommand ToCommandFromResource(CreateClientResource resource)
+    {
+        return new CreateClientCommand(resource.FirstName, resource.LastName, resource.Dni, resource.Email, resource.Age, resource.Location);
+    }
+}
