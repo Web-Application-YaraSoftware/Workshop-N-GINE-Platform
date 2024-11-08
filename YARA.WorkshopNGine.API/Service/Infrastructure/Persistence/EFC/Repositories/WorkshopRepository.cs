36009302c8c29791dbd846b1798de5eef697f29c
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 19:07:01 -0500

    feat(service): add workshop repository interface and implementation

---
+++ YARA.WorkshopNGine.API/Service/Infrastructure/Persistence/EFC/Repositories/WorkshopRepository.cs
@@ -0,0 +1,14 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Service.Domain.Repositories;
+using YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using YARA.WorkshopNGine.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace YARA.WorkshopNGine.API.Service.Infrastructure.Persistence.EFC.Repositories;
+
+public class WorkshopRepository(AppDbContext context) : BaseRepository<Workshop> (context), IWorkshopRepository
+{
+    public bool ExistsByName(string name)
+    {
+        return Context.Set<Workshop>().Any(workshop => workshop.Name.Equals(name));
+    }
+}
