a16e5d1cfb7b56c44fc2678121c54e026c0d608d
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-08 03:13:29 -0500

    feat(service): add IInterventionQueryService and its implementation
    - Added `IInterventionQueryService` interface with methods to handle queries for fetching interventions by ID and by workshop.
    - Implemented `InterventionQueryService` class with methods to handle fetching interventions by ID and by workshop.

---
+++ YARA.WorkshopNGine.API/Service/Domain/Services/IInterventionQueryService.cs
@@ -0,0 +1,11 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Service.Domain.Model.Queries;
+
+namespace YARA.WorkshopNGine.API.Service.Domain.Services;
+
+public interface IInterventionQueryService
+{
+    Task<Intervention?> Handle(GetInterventionByIdQuery query);
+    
+    Task<IEnumerable<Intervention>> Handle(GetAllInterventionsByWorkshopQuery query);
+}
