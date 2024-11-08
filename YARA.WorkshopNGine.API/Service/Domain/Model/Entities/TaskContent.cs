59c9e7ec89c1e043684a94fa5e0ffaf4dfe7a839
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-08 03:06:59 -0500

    feat(service): add Task entity with commands and value objects
    - Added `Task` entity with properties and methods for managing tasks.
    - Implemented constructors for creating tasks from commands.
    - Added `TaskStatuses` enum for task status management.

---
+++ YARA.WorkshopNGine.API/Service/Domain/Model/Entities/TaskContent.cs
@@ -0,0 +1,16 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.ValueObjects;
+
+namespace YARA.WorkshopNGine.API.Service.Domain.Model.Entities;
+
+public partial class Task
+{
+    public TaskStatuses Status { get; set; }
+    
+    //TODO: Add List Checkpoints field
+    
+    public Task()
+    {
+        Description = string.Empty;
+        Status = TaskStatuses.Pending;
+    }
+}
