a97a3ab8763b2c13e64c8c097f7576b2279e56d9
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 19:15:06 -0500

    feat(service): add WorkshopsController with CRUD operations
    - Added `WorkshopsController` class to handle HTTP requests for workshop operations.
    - Implemented `GetWorkshopById` method to fetch workshop details by a given identifier.
    - Implemented `CreateWorkshop` method to create a new workshop with a given name.
    - Implemented `UpdateWorkshop` method to update workshop details with a given identifier.

---
+++ YARA.WorkshopNGine.API/Service/Interfaces/REST/WorkshopsController.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using YARA.WorkshopNGine.API.Service.Domain.Model.Queries;
+using YARA.WorkshopNGine.API.Service.Domain.Services;
+using YARA.WorkshopNGine.API.Service.Interfaces.REST.Resources;
+using YARA.WorkshopNGine.API.Service.Interfaces.REST.Transform;
+
+namespace YARA.WorkshopNGine.API.Service.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class WorkshopsController(IWorkshopCommandService workshopCommandService, IWorkshopQueryService workshopQueryService)
+    : ControllerBase
+{
+    [HttpGet("{workshopId:long}")]
+    [SwaggerOperation(
+        Summary = "Gets a workshop by id",
+        Description = "Gets a workshop for a given identifier",
+        OperationId = "GetWorkshopById")]
+    [SwaggerResponse(200, "The workshop was found", typeof(WorkshopResource))]
+    [SwaggerResponse(404, "The workshop was not found")]
+    public async Task<IActionResult> GetWorkshopById(long workshopId)
+    {
+        var getWorkshopByIdQuery = new GetWorkshopByIdQuery(workshopId);
+        var workshop = await workshopQueryService.Handle(getWorkshopByIdQuery);
+        if (workshop == null) return NotFound();
+        var workshopResource = WorkshopResourceFromEntityAssembler.ToResourceFromEntity(workshop);
+        return Ok(workshopResource);
+    }
+    
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Creates a workshop",
+        Description = "Creates a workshop with a given name",
+        OperationId = "CreateWorkshop")]
+    [SwaggerResponse(201, "The workshop was created", typeof(WorkshopResource))]
+    [SwaggerResponse(400, "The workshop was not created")]
+    public async Task<IActionResult> CreateWorkshop([FromBody] CreateWorkshopResource createWorkshopResource)
+    {
+        var createWorkshopCommand = CreateWorkshopCommandFromResourceAssembler.ToCommandFromResource(createWorkshopResource);
+        var workshop = await workshopCommandService.Handle(createWorkshopCommand);
+        if (workshop == null) return BadRequest();
+        var resource = WorkshopResourceFromEntityAssembler.ToResourceFromEntity(workshop);
+        return CreatedAtAction(nameof(GetWorkshopById), new { workshopId = resource.Id }, resource);
+    }
+    
+    [HttpPut("{workshopId:long}")]
+    [SwaggerOperation(
+        Summary = "Updates a workshop",
+        Description = "Updates a workshop with a given identifier",
+        OperationId = "UpdateWorkshop")]
+    [SwaggerResponse(200, "The workshop was updated", typeof(WorkshopResource))]
+    [SwaggerResponse(400, "The workshop was not updated")]
+    public async Task<IActionResult> UpdateWorkshop(long workshopId, [FromBody] UpdateWorkshopResource updateWorkshopResource)
+    {
+        var updateWorkshopCommand = UpdateWorkshopCommandFromResourceAssembler.ToCommandFromResource(updateWorkshopResource);
+        var workshop = await workshopCommandService.Handle(workshopId, updateWorkshopCommand);
+        if (workshop == null) return BadRequest();
+        var resource = WorkshopResourceFromEntityAssembler.ToResourceFromEntity(workshop);
+        return Ok(resource);
+    }
+}
