a9bfea0878c51a4a0c0c9369c08baae4526a1092
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 22:58:20 -0500

    feat(service): add external IAM and profile services
    - Added `ExternalIamService` class to handle IAM-related operations.
    - Added `ExternalProfileService` class to handle profile-related operations.

---
+++ YARA.WorkshopNGine.API/Service/Application/Internal/OutboundServices/ACL/ExternalProfileService.cs
@@ -0,0 +1,14 @@
+﻿using YARA.WorkshopNGine.API.Profiles.interfaces.ACL;
+using YARA.WorkshopNGine.API.Service.Domain.Model.ValueObjects;
+
+namespace YARA.WorkshopNGine.API.Service.Application.Internal.OutboundServices.ACL;
+
+public class ExternalProfileService(IProfilesContextFacade profilesContextFacade)
+{
+    public async Task<ProfileId?> CreateProfileAsync(string firstName, string lastName, int dni, string email, int age, string location, long userId)
+    {
+        var profileId = await profilesContextFacade.CreateProfile(firstName, lastName, dni, email, age, location, userId);
+        if (profileId == 0) return await Task.FromResult<ProfileId?>(null);
+        return new ProfileId(profileId);
+    } 
+}
