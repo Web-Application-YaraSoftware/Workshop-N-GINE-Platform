c7c583dbb98992052f96a6e5261a55add4849dee
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-07 19:11:53 -0500

    feat(service): add workshop query service interface and implementation

---
+++ YARA.WorkshopNGine.API/Service/Application/Internal/QueryServices/WorkshopQueryService.cs
@@ -0,0 +1,14 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Service.Domain.Model.Queries;
+using YARA.WorkshopNGine.API.Service.Domain.Repositories;
+using YARA.WorkshopNGine.API.Service.Domain.Services;
+
+namespace YARA.WorkshopNGine.API.Service.Application.Internal.QueryServices;
+
+public class WorkshopQueryService(IWorkshopRepository workshopRepository) : IWorkshopQueryService
+{
+    public async Task<Workshop?> Handle(GetWorkshopByIdQuery query)
+    {
+        return await workshopRepository.FindByIdAsync(query.WorkshopId);
+    }
+}
---
+++ YARA.WorkshopNGine.API/Service/Domain/Services/IWorkshopQueryService.cs
@@ -0,0 +1,9 @@
+﻿using YARA.WorkshopNGine.API.Service.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Service.Domain.Model.Queries;
+
+namespace YARA.WorkshopNGine.API.Service.Domain.Services;
+
+public interface IWorkshopQueryService
+{
+    Task<Workshop?> Handle(GetWorkshopByIdQuery query);
+}
