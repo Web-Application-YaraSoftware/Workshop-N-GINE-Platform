7190f3fe80a36adf3e7d6890c6b7f0c1f7d7b669
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-08 20:20:16 -0500

    feat(repository): extend InterventionRepository with additional methods
    - Added `ExistsById` method to check if an intervention exists by its ID.
    - Added `FindByIdWithTasksAsync` method to find an intervention by ID including its tasks.
    - Added `ExistsByVehicleIdAndStatusIsPending` method to check if an intervention exists by vehicle ID and pending status.
    - Added `ExistsByMechanicLeaderIdAndTimeRange` method to check if an intervention exists by mechanic leader ID and a specific time range.

--- YARA.WorkshopNGine.API/Service/Domain/Repositories/IInterventionRepository.cs
+++ YARA.WorkshopNGine.API/Service/Domain/Repositories/IInterventionRepository.cs
@@ -6,4 +6,12 @@ namespace YARA.WorkshopNGine.API.Service.Domain.Repositories;
 public interface IInterventionRepository : IBaseRepository<Intervention>
 {
     Task<IEnumerable<Intervention>> FindAllByWorkshopIdAsync(long workshopId);
+    
+    bool ExistsById(long id);
+    
+    Task<Intervention?> FindByIdWithTasksAsync(long id);
+    
+    bool ExistsByVehicleIdAndStatusIsPending(long vehicleId);
+    
+    bool ExistsByMechanicLeaderIdAndTimeRange(long mechanicLeaderId, DateTime scheduledAt);
 }
