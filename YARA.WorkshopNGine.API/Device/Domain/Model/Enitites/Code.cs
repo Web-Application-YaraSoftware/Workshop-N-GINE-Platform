636e00af30ac5d4920ce5b8e4742882763c43d9a
Eric <erich56000@gmail.com>
2024-11-11 23:54:37 -0500

    feat(domain): add Code entity and ECodeState value object for IoT device error management
    - Implemented `Code` entity with properties for component, error code, description, last updated timestamp, and state.
    - Established association with `IotDevice` aggregate, including `IotDeviceId` for relational mapping.
    - Created `ECodeState` enum with values `Failed` and `Good` to represent the state of a code.

---
+++ YARA.WorkshopNGine.API/Device/Domain/Model/Enitites/Code.cs
@@ -0,0 +1,29 @@
+﻿using YARA.WorkshopNGine.API.Device.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Device.Domain.Model.ValueObjects;
+
+namespace YARA.WorkshopNGine.API.Device.Domain.Model.Enitites;
+
+public partial class Code
+{
+    public long Id { get; }
+    public string Component { get; private set; }
+    public string ErrorCode { get; private set; }
+    public string Description { get; private set; }
+    public DateTime lastUpdated { get; private set; }
+    public ECodeState State { get; private set; }
+    
+    public IotDevice IotDevice { get; private set; }
+    public long IotDeviceId { get; }
+    
+    public Code(string component, string errorCode, string description, ECodeState state)
+    {
+        Component = component;
+        ErrorCode = errorCode;
+        Description = description;
+        lastUpdated = DateTime.Now;
+        State = state;
+    }
+
+    
+    
+}
