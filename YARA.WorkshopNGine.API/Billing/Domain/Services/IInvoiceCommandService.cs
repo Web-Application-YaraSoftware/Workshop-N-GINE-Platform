558a3e98f69e6c1a0039e47a54e1bc738ad01b55
Eric <erich56000@gmail.com>
2024-11-12 21:29:54 -0500

    feat(domain): add command and query service interfaces
    - Introduced IInvoiceCommandService to handle invoice-related commands, including creating invoices.
    - Added IInvoiceQueryService to manage queries for retrieving invoices by workshop ID.

---
+++ YARA.WorkshopNGine.API/Billing/Domain/Services/IInvoiceCommandService.cs
@@ -0,0 +1,9 @@
+﻿using YARA.WorkshopNGine.API.Billing.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Billing.Domain.Model.Commands;
+
+namespace YARA.WorkshopNGine.API.Billing.Domain.Services;
+
+public interface IInvoiceCommandService
+{
+    Task<Invoice?> Handle(CreateInvoiceCommand command);
+}
---
+++ YARA.WorkshopNGine.API/Billing/Domain/Services/IInvoiceQueryService.cs
@@ -0,0 +1,9 @@
+﻿using YARA.WorkshopNGine.API.Billing.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Billing.Domain.Model.Queries;
+
+namespace YARA.WorkshopNGine.API.Billing.Domain.Services;
+
+public interface IInvoiceQueryService
+{
+    Task<IEnumerable<Invoice>> Handle(GetAllInvoiceByWorkshopIdQuery query);
+}
