12f110565e2e1def370f30549f65ac1e1bcf17e4
Eric <erich56000@gmail.com>
2024-11-12 21:33:27 -0500

    feat(application): add InvoiceCommandService implementation
    - Implemented InvoiceCommandService to handle the creation of invoices.
    - Added logic to create and save an invoice using the repository and unit of work.
    - Added exception handling for potential errors during invoice creation.

---
+++ YARA.WorkshopNGine.API/Billing/Application/Internal/CommandServices/InvoiceCommandService.cs
@@ -0,0 +1,29 @@
+﻿using YARA.WorkshopNGine.API.Billing.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Billing.Domain.Model.Commands;
+using YARA.WorkshopNGine.API.Billing.Domain.Repositories;
+using YARA.WorkshopNGine.API.Billing.Domain.Services;
+using YARA.WorkshopNGine.API.Shared.Domain.Repositories;
+
+namespace YARA.WorkshopNGine.API.Billing.Application.Internal.CommandServices;
+
+public class InvoiceCommandService(IInvoiceRepository invoiceRepository, IUnitOfWork unitOfWork) : IInvoiceCommandService
+{
+    public async Task<Invoice?> Handle(CreateInvoiceCommand command)
+    {
+        /**
+         * TODO: Create a facade for Amount, SubscriptionId, and PlanId, from the subscription bounded context
+         * and WorkshopId from the workshop in the service.
+         */
+        var invoice = new Invoice(command);
+        try
+        {
+            await invoiceRepository.AddAsync(invoice);
+            await unitOfWork.CompleteAsync();
+            return invoice;
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"An error occurred while creating the invoice:", e);
+        }
+    }
+}
