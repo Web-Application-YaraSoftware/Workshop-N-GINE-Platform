193bc50b0280b0066e69296d861e288e2070f479
Eric <erich56000@gmail.com>
2024-11-13 18:45:53 -0500

    refactor(api): upgrade the internal logic of the get endpoint and change the route

--- YARA.WorkshopNGine.API/CommunicationManagement/Interfaces/REST/NotificationsController.cs
+++ YARA.WorkshopNGine.API/CommunicationManagement/Interfaces/REST/NotificationsController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using YARA.WorkshopNGine.API.CommunicationManagement.Domain.Model.Queries;
 using YARA.WorkshopNGine.API.CommunicationManagement.Domain.Services;
+using YARA.WorkshopNGine.API.CommunicationManagement.Interfaces.REST.Resources;
 using YARA.WorkshopNGine.API.CommunicationManagement.Interfaces.REST.Transform;
 
 namespace YARA.WorkshopNGine.API.CommunicationManagement.Interfaces.REST;
@@ -11,11 +13,13 @@ namespace YARA.WorkshopNGine.API.CommunicationManagement.Interfaces.REST;
 [Produces(MediaTypeNames.Application.Json)]
 public class NotificationsController(INotificationQueryService notificationQueryService) : ControllerBase
 {
-    [HttpGet ("{userId}")]
-    public async Task<IActionResult> GetNotificationsByUserId(int userId)
+    [HttpGet ]
+    [SwaggerResponse( 200, "The Notifications were found", typeof(NotificationResource[]) )]
+    public async Task<IActionResult> GetNotificationsByUserId([FromQuery] int userId)
     {
-        var notifications = await notificationQueryService.Handle(new GetAllNotificationsByUserIdQuery(userId));
-        //if (notifications == null) { return NotFound(); }
+        if (userId == 0) { return BadRequest(); }
+        var getAllNotificationsByUserIdQuery = new GetAllNotificationsByUserIdQuery(userId);
+        var notifications = await notificationQueryService.Handle(getAllNotificationsByUserIdQuery);
         var resources = notifications.Select(NotificationResourceFromEntityAssembler.ToResourceFromEntity);
         return Ok(resources);
     }
