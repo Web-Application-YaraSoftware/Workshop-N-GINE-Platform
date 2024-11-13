9353d3b17443d7d2e6d45ccd52c886d5ad78a24a
Eric <erich56000@gmail.com>
2024-11-12 21:34:08 -0500

    feat(application): add InvoiceQueryService implementation
    - Implemented InvoiceQueryService to handle retrieving all invoices by workshop ID.
    - Integrated IInvoiceRepository to fetch data based on the workshop ID.

---
+++ YARA.WorkshopNGine.API/Billing/Application/Internal/QueryServices/InvoiceQueryService.cs
@@ -0,0 +1,14 @@
+﻿using YARA.WorkshopNGine.API.Billing.Domain.Model.Aggregates;
+using YARA.WorkshopNGine.API.Billing.Domain.Model.Queries;
+using YARA.WorkshopNGine.API.Billing.Domain.Repositories;
+using YARA.WorkshopNGine.API.Billing.Domain.Services;
+
+namespace YARA.WorkshopNGine.API.Billing.Application.Internal.QueryServices;
+
+public class InvoiceQueryService(IInvoiceRepository invoiceRepository) : IInvoiceQueryService
+{
+    public Task<IEnumerable<Invoice>> Handle(GetAllInvoiceByWorkshopIdQuery query)
+    { 
+        return invoiceRepository.FindAllByWorkshopIdAsync(query.WorkshopId);
+    }
+}
