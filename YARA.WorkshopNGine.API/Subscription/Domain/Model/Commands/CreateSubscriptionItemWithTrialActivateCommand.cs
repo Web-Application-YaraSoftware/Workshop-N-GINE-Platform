f059d014683cba0e3cf9c480a9bb801290e18f2f
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-14 02:23:30 -0500

    feat(commands): update CreateSubscriptionItemCommand and add new trial command - Updated `CreateSubscriptionItemCommand` to include `UserId`. - Added `CreateSubscriptionItemWithTrialActivateCommand` to support creating subscription items with trial activation.

--- YARA.WorkshopNGine.API/Subscription/Domain/Model/Commands/CreateSubscriptionItemWithTrialActivateCommand.cs
+++ YARA.WorkshopNGine.API/Subscription/Domain/Model/Commands/CreateSubscriptionItemWithTrialActivateCommand.cs
@@ -2,4 +2,4 @@ using YARA.WorkshopNGine.API.Subscription.Domain.Model.ValueObjects;
 
 namespace YARA.WorkshopNGine.API.Subscription.Domain.Model.Commands;
 
-public record CreateSubscriptionItemCommand(WorkshopId WorkshopId, UserId UserId, PlanId PlanId);
+public record CreateSubscriptionItemWithTrialActivateCommand(WorkshopId WorkshopId, UserId UserId);
