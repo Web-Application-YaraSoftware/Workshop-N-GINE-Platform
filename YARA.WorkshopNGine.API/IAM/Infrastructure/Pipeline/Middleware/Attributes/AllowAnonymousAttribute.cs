7ce3b5956bbcf8892614d3bc6abf93a20fd4cc76
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-18 06:05:06 -0500

    feat(middleware): add AllowAnonymous attribute for method-level authorization bypass - Introduced `AllowAnonymousAttribute` to enable skipping authorization for specific methods. - Simplified the management of anonymous access in the authorization middleware.

---
+++ YARA.WorkshopNGine.API/IAM/Infrastructure/Pipeline/Middleware/Attributes/AllowAnonymousAttribute.cs
@@ -0,0 +1,4 @@
+namespace YARA.WorkshopNGine.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute { }
